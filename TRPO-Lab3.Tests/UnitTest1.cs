c9679d6c0a01ce35ff0f1d5f4485eafc06574e70
Bolotov.Alexander@urfu.me <Bolotov.Alexander@urfu.me>
2021-03-16 16:30:59 +0500

    Тесты забыл

--- TRPO-Lab3.Tests/UnitTest1.cs
+++ TRPO-Lab3.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using TRPO_Lab3.Lib;
 
 namespace TRPO_Lab3.Tests
 {
@@ -12,19 +13,16 @@ namespace TRPO_Lab3.Tests
         [Test]
         public void Test1()
         {
-            TRPO_Lab3.Lib.Math_1 math_1 = new Lib.Math_1();
-            double s = math_1.Formula(10, 5);
+            double s = Math_1.Formula(10, 5);
             Assert.AreEqual(314.15,s, 0.01);
             
         }
         [Test]
         public void Test2()
         {
-            TRPO_Lab3.Lib.Math_1 math_1 = new Lib.Math_1();
-            double s = math_1.Formula(10, 5);
+            double s = Math_1.Formula(10, 5);
             Assert.Throws<System.ArgumentException>(()=> {
-                TRPO_Lab3.Lib.Math_1 math_1 = new Lib.Math_1();
-                double s = math_1.Formula(-10, 5);
+                double s = Math_1.Formula(-10, 5);
             });
         }
     }
